3b74d7ffd3c5819877261544e383d179100232b5
dickmao <none>
2019-06-16 22:55:30 -0400

    print the buffer-string on json-readtable-error

--- lisp/nnreddit.el
+++ lisp/nnreddit.el
@@ -712,8 +712,8 @@ and LVP (list of vectors of plists).  Used in the interleaving of submissions an
                               (condition-case err
                                   (setq result (json-read-from-string (buffer-string)))
                                 (json-readtable-error
-                                 (gnus-message 3 "nnreddit-rpc-wait: %s DATA: %s"
-                                               (error-message-string err) (cdr err))
+                                 (gnus-message 3 "nnreddit-rpc-wait: %s (response sofar: %s)"
+                                               (error-message-string err) (buffer-string))
                                  (setq result `(:error ,(error-message-string err)))
                                  (erase-buffer)
                                  t)
