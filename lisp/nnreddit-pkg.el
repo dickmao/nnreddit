c14bbefa7d493e8ddf65fec6a399405400610b00
dickmao <none>
2019-08-19 16:09:34 -0400

    must require url-http to advise url-http-generic-filter

--- lisp/nnreddit-pkg.el
+++ lisp/nnreddit-pkg.el
@@ -2,9 +2,9 @@
   "0.1.0"
   "Gnus Backend For Reddit"
   '((emacs "25")
-    (virtualenvwrapper "20190223.1919")
-    (json-rpc "20180104.1528")
+    (virtualenvwrapper "20190223")
+    (json-rpc "20180104")
     (dash "20190401")
     (anaphora "20180618")
-    (request "20190730"))
+    (request "20190819"))
   :url "https://github.com/dickmao/nnreddit")
