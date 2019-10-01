a087497c97e5c657943d087bb6b8758393af37f2
dickmao <none>
2019-09-30 23:38:42 -0400

    don't always reinstall package-lint

--- tools/package-lint.sh
+++ tools/package-lint.sh
@@ -12,17 +12,21 @@ INIT_PACKAGE_EL="(progn
   (package-initialize)
   (package-refresh-contents))"
 
-rm -rf "$HOME"/.emacs.d/elpa/package-lint-*
+# rm -rf "$HOME"/.emacs.d/elpa/package-lint-*
 
 # Get mainline package-lint, then replace package-lint.el with dickmao's.
 # quelpa doesn't get data/stdlib-changes.gz for whatever reason.
 "$EMACS" -Q -batch \
          --eval "$INIT_PACKAGE_EL" \
          --eval "(unless (package-installed-p (quote quelpa)) (package-install (quote quelpa)))" \
-         --eval "(package-install (quote package-lint))" \
-         --eval "(quelpa (quote (package-lint :fetcher github :repo \"dickmao/package-lint\" :branch \"datetime\")))" \
-         --eval "(let ((dir (file-name-directory (locate-library \"package-lint\")))) (delete-file (expand-file-name \"package-lint.elc\" dir)) (copy-file (expand-file-name \"package-lint/package-lint.el\" quelpa-build-dir) (expand-file-name \"package-lint.el\" dir) t))"
-
+         --eval "(unless (package-installed-p (quote package-lint)) \
+                    (package-install (quote package-lint)) \
+                    (quelpa (quote (package-lint :fetcher github :repo \"dickmao/package-lint\" :branch \"datetime\"))) \
+                    (let ((dir (file-name-directory (locate-library \"package-lint\")))) \
+                        (delete-file (expand-file-name \"package-lint.elc\" dir)) \
+                        (copy-file (expand-file-name \"package-lint/package-lint.el\" \
+                            quelpa-build-dir) (expand-file-name \"package-lint.el\" dir)
+                         t)))"
 BASENAME=$(basename "$1")
 "$EMACS" -Q -batch \
          --eval "$INIT_PACKAGE_EL" \
